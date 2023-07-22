991354c31f862ed29a8b9e76a321c370524837e8
Alexander Batenhorst <alexfbaten@gmail.com>
2023-07-22 12:13:19 -0500

    write  choice menu

--- CRUD_C_PlusPlus_App.cpp
+++ CRUD_C_PlusPlus_App.cpp
@@ -1,20 +1,70 @@
-// CRUD_C_PlusPlus_App.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+const int maxrow = 10;
+
+string EmpName[maxrow] = {};
+string EmpID[maxrow];
+
+int main () {
+    
+    std::cout << "What's your pleasure?\n";
+    int option;
+    system("CLS");
+
+    cout << "What's your pleasure?" << endl;
+    cout << "1-Create Records" << endl;
+    cout << "2-Read Records" << endl;
+    cout << "3-Update Records" << endl;
+    cout << "4-Delete Records" << endl;
+    cout << "5-Exit" << endl;
+    cout << "Enter your choice: ";
+    //cin >> choice;
+    cin.ignore();
+    
+    
+    
+    
+    
+    
+    
+    
+    bool userChoice = false;
+    string userInput;
+
+    //Print initial list of employees
+
+
+    // Repeat main loop 
+    
+    
+    
+    
+    
+    
+    // Create a text file
+  ofstream MyWriteFile("filename.txt");
+
+  // Write to the file
+  MyWriteFile << "Files can be tricky, but it is fun enough!";
+ 
+  // Close the file
+  MyWriteFile.close();
+
+  // Create a text string, which is used to output the text file
+  string myText;
+
+  // Read from the text file
+  ifstream MyReadFile("filename.txt");
+
+  // Use a while loop together with the getline() function to read the file line by line
+  while (getline (MyReadFile, myText)) {
+    // Output the text from the file
+    cout << myText;
+  }
+
+  // Close the file
+  MyReadFile.close();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
