c920e5b3bf17faff5019c0c52482b476be798a82
Alexander Batenhorst <alexfbaten@gmail.com>
2023-07-22 13:04:20 -0500

    add record and list record functions added

--- CRUD_C_PlusPlus_App.cpp
+++ CRUD_C_PlusPlus_App.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <fstream>
+
+
 using namespace std;
 
 const int maxrow = 10;
 
 string EmpName[maxrow] = {};
-string EmpID[maxrow];
+string EmpID[maxrow] = {};
+
+void AddRecord()
+{
+    char name[50];
+    char empno[5];
+
+    cin.ignore();
+
+    cout << "Employee ID. ";
+    cin.getline(empno, 5);
+    cout << "Employee Name. ";
+    cin.getline(name, 50);
+
+    for (int x = 0; x < maxrow; x++)
+    {
+        if (EmpID[x] == "\0")
+        {
+            EmpID[x] = empno;
+            EmpName[x] = name;
+
+            break;
+        }
+    }
+}
+
+void ListRecord()
+{
+    system("CLS");
+    cout << "Current Record(s)" << endl;
+    cout << "=================================" << endl;
+
+    int counter = 0;
+    cout << " No.   |    ID     NAME    " << endl << "-----------------------------------\n";
+    for (int x = 0; x < maxrow; x++)
+    {
+        if (EmpID[x] != "\0")
+        {
+            counter++;
+            cout << "   " << counter << "      " << EmpID[x] << "         " << EmpName[x] << endl;
+
+            break;
+        }
+    }
+
+}
 
 int main () {
     
@@ -14,57 +61,31 @@ int main () {
     int option;
     system("CLS");
 
-    cout << "What's your pleasure?" << endl;
-    cout << "1-Create Records" << endl;
-    cout << "2-Read Records" << endl;
-    cout << "3-Update Records" << endl;
-    cout << "4-Delete Records" << endl;
-    cout << "5-Exit" << endl;
-    cout << "Enter your choice: ";
+    do {
+        cout << "1-Create Records" << endl;
+        cout << "2-Read Records" << endl;
+        cout << "3-Update Records" << endl;
+        cout << "4-Delete Records" << endl;
+        cout << "5-Search all Records" << endl;
+        cout << "6-Exit & Save to Textfile" << endl;
+        cout << "==================================" << endl;
+
+        cout << "Select Option >>";
+        cin >> option;
+
+        switch (option)
+        {
+        case 1: AddRecord();
+            system("CLS");
+            break;
+        
+        case 5: ListRecord();
+            break;
+        }
+
+       
+    } while (option != 6);
     //cin >> choice;
-    cin.ignore();
+      
     
-    
-    
-    
-    
-    
-    
-    
-    bool userChoice = false;
-    string userInput;
-
-    //Print initial list of employees
-
-
-    // Repeat main loop 
-    
-    
-    
-    
-    
-    
-    // Create a text file
-  ofstream MyWriteFile("filename.txt");
-
-  // Write to the file
-  MyWriteFile << "Files can be tricky, but it is fun enough!";
- 
-  // Close the file
-  MyWriteFile.close();
-
-  // Create a text string, which is used to output the text file
-  string myText;
-
-  // Read from the text file
-  ifstream MyReadFile("filename.txt");
-
-  // Use a while loop together with the getline() function to read the file line by line
-  while (getline (MyReadFile, myText)) {
-    // Output the text from the file
-    cout << myText;
-  }
-
-  // Close the file
-  MyReadFile.close();
 }
